c65f5d4c59a116dd545f2009f3cd19554bf9f335
Grant Ellison <grant@Grants-MacBook-Pro.local>
2024-05-27 09:47:13 -0700

    special relativity

---
+++ theoretical_minimum/special_relativity_classical_field_theory/utils.py
@@ -0,0 +1,8 @@
+import numpy as np, sympy as sp
+from sympy import *
+from IPython.display import display, Math
+import itertools
+import matplotlib.pyplot as plt
+
+def lprint(s):
+    return display(Math(s))
